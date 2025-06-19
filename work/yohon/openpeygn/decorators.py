394d2b0bcdde95b8aa3cc24cfca0fc1e265c52f9
yeonghun hong <yeong.hong@nzero.kr>
2025-06-19 11:28:22 +0900

    20250619_04 * decorators 개선

--- work/yohon/openpeygn/decorators.py
+++ work/yohon/openpeygn/decorators.py
@@ -9,6 +9,10 @@ def peygn_client(base_url):
     return decorator
 
 def api_mapping(method, path):
+    method = method.upper()
+    if method is not "GET" or method is not "POST" or method is not "PUT" or method is not "DELETE":
+        raise ValueError
+
     def decorator(func):
         func._api_method = method
         func._api_path   = path
@@ -17,46 +21,32 @@ def api_mapping(method, path):
     return decorator
 
 def get_mapping(method, path):
-    if method is None:
-        method = "GET"
-
     def decorator(func):
-        func._api_method = method
+        func._api_method = "GET"
         func._api_path   = path
         return func
 
     return decorator
 
-def post_mapping(method, path):
-    if method is None:
-        method = "POST"
-
+def post_mapping(path):
     def decorator(func):
-        func._api_method = method
+        func._api_method = "POST"
         func._api_path   = path
         return func
 
     return decorator
 
-def put_mapping(method, path):
-    if method is None:
-        method = "PUT"
-
+def put_mapping(path):
     def decorator(func):
-        func._api_method = method
+        func._api_method = "PUT"
         func._api_path   = path
         return func
 
     return decorator
 
-def delete_mapping(method, path):
-    if method is None:
-        method = "DELETE"
-    elif method != "DELETE":
-        raise ValueError
-
+def delete_mapping(path):
     def decorator(func):
-        func._api_method = method
+        func._api_method = "DELETE"
         func._api_path   = path
         return func
 
